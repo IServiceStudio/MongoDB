100f93157b1f777605e910cdebb24af4538d43f5
Feng Lv <lv.feng@outlook.com>
2021-11-24 15:42:46 +0800

    初始化代码

---
+++ ConsoleApp/MongoAPI/Controllers/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoAPI.Models;
+using MongoAPI.Service;
+
+namespace MongoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        #region 成员变量及其属性
+
+        private readonly BookService _bookService;
+
+        #endregion
+
+        #region 构造函数
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        #endregion
+
+        #region 公共函数
+
+        [HttpGet]
+        public List<Book> Get() => _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public Book Get(string id) => _bookService.Get(id);
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+            return CreatedAtRoute("GetBook", book.Id.ToString(), book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        { 
+            var book = _bookService.Get(id);
+            if(book==null) return NotFound();
+            _bookService.Update(id,bookIn);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+            if (book == null) return NotFound();
+            _bookService.Remove(id);
+            return NoContent();
+        }
+
+        #endregion
+
+        #region 私有函数
+
+        #endregion
+    }
+}
---
+++ ConsoleApp/MongoAPI/Service/BookService.cs
@@ -0,0 +1,47 @@
+﻿using MongoAPI.Models;
+using MongoDB.Driver;
+
+namespace MongoAPI.Service
+{
+    public class BookService
+    {
+        #region 成员变量及其属性
+
+        private readonly IMongoCollection<Book> _books;
+
+        #endregion
+
+        #region 构造函数
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        #endregion
+
+        #region 公共函数
+
+        public List<Book> Get() => _books.Find(book => true).ToList();
+
+        public Book Get(string id) => _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) => _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(string id) => _books.DeleteOne(book => book.Id == id);
+
+        #endregion
+
+        #region 私有函数
+
+        #endregion
+    }
+}
